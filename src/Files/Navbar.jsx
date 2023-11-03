d982720b5a24c59643940aff53cc4b83424fb93b
Deepak Kumar Jaiswal <ranjansharma.ind@gmail.com>
2023-11-03 23:07:29 +0530

    add

--- src/Files/Navbar.jsx
+++ src/Files/Navbar.jsx
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Container, Offcanvas, Navbar, Nav } from "react-bootstrap";
 import "./Style.css";
 
-function Navbar() {
-
+function Header() {
   const [show, setShow] = useState(false);
 
   const offsetValue = -56;
 
-  const toggleoffCanvas = () =>{
+  const toggleOffCanvas = () => {
     setShow((show) => !show);
   };
 
   return (
     <div>
-      <nav className="navbar navbar-light bg-transparent">
+      {/* <nav className="navbar navbar-light bg-transparent">
         <div className="container-fluid">
           <NavLink className="navbar-brand" to="/">
             portfolio
@@ -32,8 +32,6 @@ function Navbar() {
             className="offcanvas offcanvas-end"
             tabindex="-1"
             id="offcanvasDarkNavbar"
-            show={show}
-            aria-hidden={{toggleoffCanvas}}
             aria-labelledby="offcanvasDarkNavbarLabel"
           >
             <div className="offcanvas-header">
@@ -50,7 +48,7 @@ function Navbar() {
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
-                  <NavLink className="nav-link" aria-current="page" to="/" offsetValue={offsetValue}>
+                  <NavLink className="nav-link" aria-current="page" to="/">
                     Home
                   </NavLink>
                 </li>
@@ -73,9 +71,91 @@ function Navbar() {
             </div>
           </div>
         </div>
-      </nav>
+      </nav> */}
+      <Navbar
+        className="navbar navbar-light bg-transparent container"
+        collapseOnSelect
+        bg="dark"
+        variant="dark"
+        expand={false}
+      >
+        <Container fluid>
+          <Navbar.Brand className="navbar-brand" href="#">
+            portfolio
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="offcanvasNavbar"
+            className="navbar-toggler"
+            onClick={toggleOffCanvas}
+          />
+          <Navbar.Offcanvas
+            className="offcanvas"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+            show={show}
+            onHide={toggleOffCanvas}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel"></Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body className="offcanvas-body">
+              <Nav className="justify-content-end flex-grow-1 pe-3 offcanvas--menu offcanvas-body">
+                <NavLink
+                  activeClass="active"
+                  to="/"
+                  spy={true}
+                  smooth={true}
+                  offset={offsetValue}
+                  duration={500}
+                  className="nav-link"
+                  onClick={toggleOffCanvas}
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  activeClass="active"
+                  to="/About"
+                  spy={true}
+                  smooth={true}
+                  offset={offsetValue}
+                  duration={500}
+                  onClick={toggleOffCanvas}
+                  className="nav-link"
+                >
+                  About
+                </NavLink>
+                <NavLink
+                  activeClass="active"
+                  to="/Whatido"
+                  spy={true}
+                  smooth={true}
+                  offset={offsetValue}
+                  duration={500}
+                  onClick={toggleOffCanvas}
+                  className="nav-link"
+                >
+                  My Work
+                </NavLink>
+                <NavLink
+                  activeClass="active"
+                  to="/Letstalk"
+                  spy={true}
+                  smooth={true}
+                  offset={offsetValue}
+                  duration={500}
+                  onClick={toggleOffCanvas}
+                  className="nav-link"
+                >
+                  Contact
+                </NavLink>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </div>
   );
 }
 
-export default Navbar;
+export default Header;
